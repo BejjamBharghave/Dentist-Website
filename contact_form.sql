64fd5f73c90834bf042245e498a9b49f4419f0ed
BejjamBharghave <111566967+BejjamBharghave@users.noreply.github.com>
2023-03-23 17:20:42 +0530

    Update contact_form.sql

--- contact_form.sql
+++ contact_form.sql
@@ -1,10 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 24, 2022 at 10:50 AM
--- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
